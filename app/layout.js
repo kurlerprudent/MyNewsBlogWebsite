db172a7dcabd5581da9bf1f264d6cc5786394eaa
kurlerprudent <osarkodie@st.ug.edu.gh>
2024-09-28 11:49:53 +0000

    initial commit

--- app/layout.js
+++ app/layout.js
@@ -1,6 +1,7 @@
 import localFont from "next/font/local";
 import "./globals.css";
 
+
 const geistSans = localFont({
   src: "./fonts/GeistVF.woff",
   variable: "--font-geist-sans",
@@ -21,7 +22,9 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body className={`${geistSans.variable} ${geistMono.variable}`}>
+
         {children}
+ 
       </body>
     </html>
   );
